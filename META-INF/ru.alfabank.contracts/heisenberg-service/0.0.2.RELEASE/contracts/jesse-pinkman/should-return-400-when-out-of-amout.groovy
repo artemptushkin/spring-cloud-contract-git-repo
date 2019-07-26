9f2c015514a19f83e4440e903c9e954a0005e65c
Artem Ptushkin <aptushkin@alfabank.ru>
2019-07-26 16:40:11 +0300

    Updating project [ru.alfabank.contracts:heisenberg-service:0.0.2.RELEASE] with stubs

---
+++ META-INF/ru.alfabank.contracts/heisenberg-service/0.0.2.RELEASE/contracts/jesse-pinkman/should-return-400-when-out-of-amout.groovy
@@ -0,0 +1,22 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+	request {
+		method 'POST'
+		url '/blueCrystals/create'
+		headers {
+			header 'Content-Type' : 'application/json;charset=UTF-8'
+		}
+		body(
+			amount: 1000
+		)
+		bodyMatchers {
+			jsonPath('$.amount', byEquality())
+		}
+	}
+	response {
+		status 400
+	}
+}
+
+
